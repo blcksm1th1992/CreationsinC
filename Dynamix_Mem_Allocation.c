519c6e44ad0fe691e0879c24984c5e8911914068
blcksm1th1992 <42398321+blcksm1th1992@users.noreply.github.com>
2019-12-17 14:06:08 -0600

    Update Dynamix_Mem_Allocation.c

--- Dynamix_Mem_Allocation.c
+++ Dynamix_Mem_Allocation.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
 	int size=0;
 	int *integer = (int*)malloc(sizeof(int));
-	int i, j, n, *tmp = integer;
+	int i, j, n;
 
 	do
 	{
-
 		printf("__________________________________________________________________________________________________________\nWelcome to the Stack Program, so do you want to:\n\n 1.) Add a Value to the Stack? \n 2.) Process a node in the stack?\n 3.) Quit the Program? \n\n");
-		
-			if (integer[0] == 0 || size == 0 )
+
+			if (integer[0] == NULL || size == NULL )
 			{
 				printf("The Stack is Currently empty!\n\n");
 			}
 
-			else if (integer[0] != 0)
+			else if (integer[0] != NULL)
 			{
 				printf("The Current DATA Stack:\n");
 				for (i = 0; i < size; ++i)
@@ -36,30 +34,31 @@ int main()
 		switch (n)
 		{
 			case 1: size = size + 1;
-					tmp = (int*)realloc(tmp, ((size+1) * sizeof(int)));
+					integer = (int*)realloc(integer, (sizeof(int)*size));
 					printf("Now add a value to the stack: ");
 					scanf("%d", &j);
-					tmp[i++] = j;
+					if(size == 1){integer[0] = j;}
+					else{integer[i++] = j;}
 					puts(" ");
 					break;
 
 			case 2: if (i >= size && size > 0)
 					{
+                        integer = (int*)realloc(integer, (sizeof(int)*size));
 						i = i-1;
-						printf("Now procesing %d!!!\n", tmp[i]);
+						printf("Now processing %d!!!\n", integer[i]);
 						size = size - 1;
-						tmp = (int*)realloc(tmp, sizeof(int)*(size));
 					}
-					break;	
+					break;
 
-			case 3: printf("Freeing all the memory. Toksha, Have a Good Day!\n"); 
-					break;		
+			case 3: printf("Freeing all the memory. Toksha, Have a Good Day!\n");
+					break;
 			default: printf("ERROR\n");
 					 break;
 		}
 	}while(n != 3);
 
-
-	free(tmp);
+	free(integer);
 	return 0;
 }
+
